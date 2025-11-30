84bf9cb0ef61943f358b0c92316159bf899ccf50
Emanoel Elon <emanoelefgms@gmail.com>
2025-11-30 15:27:33 -0300

    Update index.php

--- views/index.view.php
+++ views/index.view.php
@@ -1,12 +1,16 @@
 <div class="content">
     <div>
-        <div id="relogio"></div>
+        <div id="timer"></div>
         <p>data de hoje</p>
         <div class="circle">
             <img src="https://img.icons8.com/?size=100&id=100036&format=png&color=ffffff" alt="">
             <span>REGISTRAR PONTO</span>
         </div>
     </div>
+
+    <!-- <form action="" method="post">
+        <input type="text" name="timer" id="timer">
+    </form> -->
 </div>
 
 <script>
@@ -16,8 +20,11 @@
         let minutos = agora.getMinutes().toString().padStart(2, '0');
         let segundos = agora.getSeconds().toString().padStart(2, '0');
 
-        document.getElementById('relogio').textContent =
+        document.getElementById('timer').textContent =
             `${horas}:${minutos}:${segundos}`;
+
+            // document.getElementById('timer').value =
+            // `${horas}:${minutos}:${segundos}`;
     }
 
     // Atualiza a cada 1 segundo
